06e14934656e14680f8c96abc46fda6cb23419cc
yuta <yuta1110@hotmail.com>
2019-03-17 22:57:48 +0900

    20190317
    P.91 レイアウト

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,25 +1,17 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-    body {font-size:16pt; color:#999;}
-    h1 {
-        font-size:100pt; text-align:right; color#f6f6f6;
-        margin:-50px 0px -100px 0px;
-    }
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    <p>&#064;forディレクティブ例</p>
-    @foreach( $datas as $item )
-        @if( $loop->first )
-            <p>※データ一覧</p><ul>
-        @endif
-        <li>No,{{$loop->iteration}}. {{$item}}</li>
-        @if( $loop->last )
-            </ul><p>-------ここまで</p>
-        @endif
-    @endforeach
-</body>
-</html>
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文のコンテンツです。</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+    copyright 2019.
+@endsection
