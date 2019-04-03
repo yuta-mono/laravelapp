b031676a5428b4fe1a68207e7c0cad8cfa5380d0
yuta <yuta1110@hotmail.com>
2019-04-03 22:53:45 +0900

    20190403-2
    P118

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'HelloController@index')->middleware(HelloMiddleware::class);
+Route::get('hello', 'HelloController@index');
 Route::post('hello', 'HelloController@post');
 
 
