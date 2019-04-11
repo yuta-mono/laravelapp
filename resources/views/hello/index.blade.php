c743bbbb1312e0692419180d6b68e6e18de579e1
yuta <yuta1110@hotmail.com>
2019-04-11 22:54:35 +0900

    20190411
    P127

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -8,9 +8,23 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです</p>
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+        <div>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
+    <table>
+        <form action="./hello" method="post">
+            {{ csrf_field() }}
+            <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+            <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+            <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
+            <tr><th></th><td><input type="submit" value="send"></td></tr>
+        </form>
+    </table>
 @endsection
 
 @section('footer')
