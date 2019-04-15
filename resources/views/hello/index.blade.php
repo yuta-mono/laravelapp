112f368847b4152aa518210cb577d37c2e7a5065
yuta <yuta1110@hotmail.com>
2019-04-15 22:14:55 +0900

    20190415-1
    P130

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,17 +10,24 @@
 @section('content')
     <p>{{$msg}}</p>
     @if (count($errors) > 0)
-        <div>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </div>
+        <p>入力に誤りがあります。再入力してください。</p>
     @endif
     <table>
         <form action="./hello" method="post">
             {{ csrf_field() }}
+            @if ($errors->has('name'))
+                <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+            @endif
             <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+
+            @if ($errors->has('mail'))
+                <tr><th>ERROR</th><td>{{$errors->first('mail')}}</td></tr>
+            @endif
             <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+
+            @if ($errors->has('age'))
+                <tr><th>ERROR</th><td>{{$errors->first('age')}}</td></tr>
+            @endif
             <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
             <tr><th></th><td><input type="submit" value="send"></td></tr>
         </form>
